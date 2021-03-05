470f215cd64c25d9ebb9fad347a11c5c6bda55ee
samm021 <mohammed.ismail.pribadi@gmail.com>
2021-03-05 16:31:39 +0700

    dicee flutter 2nd challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,26 +16,51 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void randomizeDiceNumber() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset('images/dice1.png'),
-              )
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: (){
+                setState(() {
+                  randomizeDiceNumber();
+                });
+              },
+            ),
           ),
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset('images/dice2.png'),
-              )
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: (){
+                setState(() {
+                  randomizeDiceNumber();
+                });
+              },
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+
+// class DicePage extends StatelessWidget {
+// }
